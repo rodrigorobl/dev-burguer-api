7d5da9c91adcaf28aa3e07b709b760bfd76b2b46
rodrigorobl <rodrigo.augusto.r21@gmail.com>
2025-12-22 23:22:12 -0300

    added cors origin configuration

--- src/app.js
+++ src/app.js
@@ -6,9 +6,14 @@ import 'dotenv/config';
 
 const app = express();
 
-app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+  }),
+);
+
 app.use('/product-file', fileRouteConfig);
 app.use('/category-file', fileRouteConfig);
 
